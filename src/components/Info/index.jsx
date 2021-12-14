d9e8476610c89574743f36167f176fd2488091d7
Isaias <isaiashsobral@gmail.com>
2021-12-14 19:54:12 -0300

    Added verification of data to elements

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -9,10 +9,11 @@ function Info({data}) {
     <Animated animationIn="bounceInUp" className='info'>
         <h2>CEP: {data?.cep}</h2>
 
-        <h3>Rua: {data?.logradouro}</h3>
-        <h3>Bairro: {data?.bairro}</h3>
-        <h3>Cidade: {data?.localidade}</h3>
-        <h3>Distrito: {data?.uf}</h3>
+        {data.logradouro && <h3>Rua: {data?.logradouro}</h3>}
+        {data.complemento && <h3>Complemento: {data?.complemento}</h3>}
+        {data.bairro && <h3>Bairro: {data?.bairro}</h3>}
+        {data.localidade && <h3>Cidade: {data?.localidade}</h3>}
+        {data.uf && <h3>Distrito: {data?.uf}</h3>}
     </Animated>
   );
 }
